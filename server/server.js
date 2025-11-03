2ded660f8cba2d7bda614adb5576cbf33b7f09db
Rodgers Mugagga <rodgersmugagga84@gmail.com>
2025-11-03 12:13:26 +0300

    Add vercel.json for Express backend routing

--- server/server.js
+++ server/server.js
@@ -41,6 +41,9 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app; 
